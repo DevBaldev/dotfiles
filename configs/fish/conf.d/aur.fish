2ef1b11283e285f7ea4afed6f67b297eefcd0821
DevBaldev <triggerflax@gmail.com>
2025-11-26 13:36:00 +0530

    26/11/25 13:36

--- configs/fish/conf.d/aur.fish
+++ configs/fish/conf.d/aur.fish
@@ -1,9 +1,6 @@
 set -U aurhelper paru
 
 abbr --add pr '$aurhelper -Rncus'
-
 abbr --add pi '$aurhelper -S --needed'
-
 abbr --add pu '$aurhelper -Syu --noconfirm'
-
 abbr --add pc '$aurhelper -Rncus $($aurhelper -Qtdq) --noconfirm'
